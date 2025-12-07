773bc2a2b16bf263b37712c14a150ce446099aa3
Davi Aires <davi.aires03@aluno.ifce.edu.br>
2025-12-07 20:05:33 -0300

    Funcionalidades do tabuleiro incorporadas ao menu. Novas telas. Comentários.

--- pilha_torre_de_hanoi/menu_inicial.c
+++ pilha_torre_de_hanoi/menu_inicial.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//////////////////////
+//Funções de suporte//
+//////////////////////
+
+//Identifica o sistema (Windows ou Unix) e aplica o comando correspondente para apagar o terminal.
+//Usada nas transições de tela.
 void limparTela() {
-#ifdef _WIN32
+    #ifdef _WIN32
     system("cls");
-#else
+    #else
     system("clear");
-#endif
+    #endif
 }
 
+//Identifica o sistema (Windows ou Unix) e aplica os comandos correspondentes para ler um caracter qualquer e descartá-lo.
+//Usada quando é necessária uma confirmação do jogador.
 void pressioneQualquerTecla() {
-    
     #ifdef _WIN32
     fflush(stdout);
     getch();
@@ -21,6 +28,100 @@ void pressioneQualquerTecla() {
     #endif
 }
 
+//Retorna o resultado da potenciação da base pelo expoente passados como argumentos.
+//Usada para calcular o número mínimo de jogadas em cada fase
+int potencia(int base, int expoente) {
+    int resultado = 1;                   
+    for (int i = 0; i < expoente; i++) {
+        resultado *= base;
+    }
+    return resultado;
+}
+
+////////////////////
+//Funções gráficas//
+////////////////////
+
+//Imprime uma peca com base no número passado como argumento.
+//Cada peça tem um tamanho e um número únicos que a identificam visualmente.
+void imprimePeca(int peca){
+    switch (peca) {
+        case 0:                               
+        printf("          -0-          ");
+        break;
+        case 1:
+        printf("         --1--         ");
+        break;
+        case 2:
+        printf("        ---2---        ");
+        break;
+        case 3:
+        printf("       ----3----       ");
+        break;
+        case 4:
+        printf("      -----4-----      ");
+        break;
+        case 5:
+        printf("     ------5------     ");
+        break;
+        case 6:
+        printf("    -------6-------    ");
+        break;
+        case 7:
+        printf("   --------7--------   ");
+        break;
+        case 8:
+        printf("  ---------8---------  ");
+        break;
+        case 9:
+        printf(" ----------9---------- ");
+        break;
+        default:
+        printf("           |           ");
+    }
+}
+
+//Imprime o estado atual do tabuleiro, com as peças ordenadas em suas respectivas hastes.
+//Usada dentro do jogo, no início e sempre que o jogador faz uma jogada.
+void tabuleiro(){
+    int haste1[10] = {-1, -1, -1, -1, -1, -1, 0, 2, 4, 5};
+    int haste2[10] = {-1, -1, -1, -1, -1, -1, -1, 1, 3, 6};
+    int haste3[10] = {-1, -1, -1, -1, -1, -1, -1, 7, 8, 9};
+    
+    printf("\n           |                      |                      |           \n");
+    for(int i = 0; i < 10; i++){
+        imprimePeca(haste1[i]);
+        imprimePeca(haste2[i]);
+        imprimePeca(haste3[i]);
+        printf("\n");
+    }
+    printf("=====================================================================\n");
+    printf("          [1]                    [2]                    [3]          \n\n");
+}
+
+//Imprime a tela do jogo, identificando o número da fase, o número mínimo de jogadas e
+//o número de jogadas realizadas até o momento.
+//Usada dentro do jogo, no início e sempre que o jogador faz uma jogada.
+void fase(int faseAtual){
+    int quantidadeInicialDiscos = faseAtual + 2;
+    int numeroMinimoMovimentos = potencia(2, quantidadeInicialDiscos) - 1;
+    int contaMovimentos = 0;
+    
+    printf("Fase: %d\n", faseAtual);
+    printf("Número Mínimo de Movimentos: %d                   Seus movimentos: %d  \n", numeroMinimoMovimentos, contaMovimentos);
+    tabuleiro();
+}
+
+/////////////////////
+//Controle de Fluxo//
+/////////////////////
+
+//Controla o estado do jogo.
+void jogo (int faseAtual){
+    fase(faseAtual);
+}
+
+//Exibe o menu inicial e direciona o jogador através das opções.
 int menu_inicial(int opcao){
     printf("=============================================\n");
     printf("              \033[1;36mTORRE DE HANOI\033[0m\n");
@@ -34,10 +135,10 @@ int menu_inicial(int opcao){
     printf("Escolha uma opção: ");
     scanf("%d", &opcao);
     limparTela();
-
+    
     switch (opcao) {
         case 1:
-        printf("<Novo Jogo>\n");
+        jogo(1);
         break;
         case 2:
         printf("<Escolher Dificuldade>\n");
@@ -78,9 +179,7 @@ int main() {
     printf("============================================================\n");
     printf("\nPressione qualquer tecla para iniciar");
     pressioneQualquerTecla();
-    limparTela();        
-    do {
-        opcao = menu_inicial(opcao);
-        } while (opcao != 0);   
+    limparTela();
+    opcao = menu_inicial(opcao);
     return 0;
 }
